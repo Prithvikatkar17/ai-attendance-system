108c0294607140029b66c71d768e2d19b38de8ef
Prithvi Katkar <prithvirajkatkar17@gmail.com>
2026-08-09 18:31:15 +0530

    fix : typo

--- src/screens/student_screen.py
+++ src/screens/student_screen.py
@@ -68,10 +68,10 @@ def student_dashboard():
         stats = stats_map.get(sid,{"total":0, "attended": 0} )
         def unenroll_button(sid=sid, sub=sub):
             if st.button("Unenroll from this course", type='tertiary', width='stretch', 
-                 icon=':material/delete_forever:', key=f"unenroll_{sid}"):
+                    icon=':material/delete_forever:', key=f"unenroll_{sid}"):
                 unenroll_student_to_subject(student_id, sid)
-            st.toast(f"Unenrolled from {sub['name']} successfully!")
-            st.rerun()
+                st.toast(f"Unenrolled from {sub['name']} successfully!")  
+            #st.rerun()
 
         with cols[i % 2]:
 
